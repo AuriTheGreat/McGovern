dfd7ef35bd222fa89aa23af7bb2357e2e3897bc1
aurimas113 <aurimas113@gmail.com>
2022-07-06 15:23:52 +0300

    Basic main menu

--- McGovern/McGovern/Main.py
+++ McGovern/McGovern/Main.py
@@ -1,6 +1,7 @@
 import ScenarioHandler
 import ResultHandler
 import os
+import time
 import pygame
 from pygame.locals import *
 
@@ -22,6 +23,23 @@ def getscenario(scenarios):
             return answer
         else:
             print("Wrong input: scenario name needed.")
+
+class Rectangle():
+    def __init__(self, x, y, width, height, color):
+            self.x = x
+            self.y = y
+            self.width = width
+            self.height = height
+            self.color = color
+
+            objects.append(self)
+
+            self.Surface = pygame.Surface((self.width, self.height))
+            self.Surface.fill(self.color)
+            self.Rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def process(self):
+        screen.blit(self.Surface, self.Rect)
 
 class Image():
     def __init__(self, x, y, width, height, img):
@@ -49,7 +67,7 @@ class Button():
         self.onePress = onePress
 
         self.fillColors = {
-            'normal': '#ffffff',
+            'normal': '#250045',
             'hover': '#666666',
             'pressed': '#333333',
         }
@@ -57,7 +75,7 @@ class Button():
         self.buttonSurface = pygame.Surface((self.width, self.height))
         self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+        self.buttonSurf = font.render(buttonText, True, (255, 255, 255))
 
         self.alreadyPressed = False
 
@@ -66,6 +84,7 @@ class Button():
     def process(self):
 
         mousePos = pygame.mouse.get_pos()
+        global isclicked
         
         self.buttonSurface.fill(self.fillColors['normal'])
         if self.buttonRect.collidepoint(mousePos):
@@ -78,32 +97,45 @@ class Button():
                     self.onclickFunction()
 
                 elif not self.alreadyPressed:
-                    self.onclickFunction()
-                    self.alreadyPressed = True
+                    if isclicked==False:
+                        self.onclickFunction()
+                        self.alreadyPressed = True
+                        isclicked=True
 
             else:
                 self.alreadyPressed = False
+                isclicked=False
 
         self.buttonSurface.blit(self.buttonSurf, [
             self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
             self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
         ])
+
         screen.blit(self.buttonSurface, self.buttonRect)
 
 
 def mainmenu():
     objects.clear()
-    Button(width/2-200, 30, 400, 100, 'Button One (onePress)', choosescenario)
-    Image(width/2-200, 10, 200, 50, 'gfx/title.png')
+    Rectangle(0,screen_width/(1200/250),screen_width,screen_height, '#D4FFFD')
+    button_size_x, button_size_y = screen_width/3, screen_height/(700/90)
+    Button(screen_width/2-(button_size_x/2), screen_width/(1200/300), button_size_x, button_size_y, 'Play', choosescenario)
+    Button(screen_width/2-(button_size_x/2), screen_width/(1200/400), button_size_x, button_size_y, 'Options', options)
+    Button(screen_width/2-(button_size_x/2), screen_width/(1200/500), button_size_x, button_size_y, 'Quit', quit)
+    Image(0, screen_width/(1200/10), screen_width, screen_width/4.8, 'gfx/title.png')
 
 
 def choosescenario():
     objects.clear()
-    Button(width/2-200, 130, 400, 100, 'Button One (onePress)', mainmenu)
+    Button(screen_width/2-200, 130, 400, 100, 'Go back', mainmenu)
     for i in scenarios:
-        Button(width/2-200, 230, 400, 100, i)
+        Button(screen_width/2-200, 230, 400, 100, i)
 
+def options():
+    objects.clear()
+    Button(screen_width/2-200, 130, 400, 100, 'Go back', mainmenu)
 
+def quit():
+    pygame.quit()
 
 
 if __name__ == "__main__":
@@ -115,14 +147,17 @@ if __name__ == "__main__":
     results=ResultHandler.main(scenario)
 
     pygame.init()
-    fps = 60
+    pygame.display.set_caption('McGovern')
+    pygame.display.set_icon(pygame.image.load('gfx/icon.png'))
+    fps = 30
     fpsClock = pygame.time.Clock()
-    width, height = 640, 480
-    screen = pygame.display.set_mode((width, height))
-    
-    font = pygame.font.SysFont('Arial', 40)
+    screen_width, screen_height = 1200, 700
+    screen = pygame.display.set_mode((screen_width, screen_height))
+
+    font = pygame.font.SysFont('Arial', int(screen_height/(700/40)))
 
     objects = []
+    isclicked=False
 
     mainmenu()
     
@@ -131,7 +166,7 @@ if __name__ == "__main__":
         screen.fill((20,20,20))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                quit()
     
         for object in objects:
             object.process()
